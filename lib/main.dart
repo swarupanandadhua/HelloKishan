fdfb57d83e05841c42a52619cab28c052b7e2c7d
Swarupananda Dhua <swarupanandadhua@gmail.com>
2020-07-23 16:46:24 +0530

    AndroidManifest: INTERNET permission added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:farmapp/screens/home/home.dart';
+import 'package:farmapp/screens/search/search.dart';
 import 'package:farmapp/services/authentication.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -22,12 +22,12 @@ class FarmApp extends StatelessWidget {
         home: SplashScreen(
           seconds: 3,
           // navigateAfterSeconds: Wrapper(),
-          // navigateAfterSeconds: SearchScreen(),
+          navigateAfterSeconds: SearchScreen(),
           // navigateAfterSeconds: PostRequirementScreen(),
           // navigateAfterSeconds: OTPLoginScreen(),
           // navigateAfterSeconds: AccountScreen(),
           // navigateAfterSeconds: HistoryScreen(),
-          navigateAfterSeconds: HomeScreen(),
+          // navigateAfterSeconds: HomeScreen(),
           image: Image.asset('images/app_logo.jpg'),
           backgroundColor: Colors.white,
           photoSize: 130.0,

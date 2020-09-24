37e587e7a02478b163be6feaa23e88ad459aed1d
Swarupananda Dhua <swarupan@synopsys.com>
2020-09-24 23:24:27 +0530

    Helping Arpita

--- lib/Screens/Common/Timestamp.dart
+++ lib/Screens/Common/Timestamp.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 String getTimeStamp(Timestamp timestamp) {
@@ -11,5 +12,17 @@ String getTimeStampBengali(Timestamp timestamp) {
   String s = getTimeStamp(timestamp);
   // TODO: ARPITA: Manipulate s to convert it to a bengali timestamp
 
+  String month = s.substring(13, 15); // why 13-15 ???
+  String bengaliMonth;
+  switch (month) {
+    case 'Jan':
+      bengaliMonth = 'জানুয়ারি';
+      break;
+    case 'Feb':
+      bengaliMonth = 'ফেব্রুয়ারি';
+      break;
+    // TODO
+  }
+  debugPrint(bengaliMonth);
   return s;
 }

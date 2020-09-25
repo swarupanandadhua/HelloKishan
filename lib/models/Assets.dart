c4785468d09ad2e54d755100e18bec11303e9641
Swarupananda Dhua <swarupan@synopsys.com>
2020-09-25 10:44:28 +0530

    Deleting ...

--- lib/models/Assets.dart
+++
@@ -1,5 +0,0 @@
-const String ASSET_APP_LOGO = 'assets/images/app_logo.jpg';
-const String ASSET_LOADING = 'assets/images/loading.gif';
-const String ASSET_ACCOUNT = 'assets/images/account.png';
-const String ASSET_RED_CROSS = 'assets/images/red_cross.png';
-const String ASSET_GREEN_TICK = 'assets/images/green_tick.png';

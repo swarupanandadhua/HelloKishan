93471cb5082e526a5697ae972219949de69ebed5
Arpita Kundu <arpita@Arpita.local>
2020-09-25 10:11:19 +0530

    adding Bengali months

--- lib/Screens/Common/Timestamp.dart
+++ lib/Screens/Common/Timestamp.dart
@@ -12,7 +12,7 @@ String getTimeStampBengali(Timestamp timestamp) {
   String s = getTimeStamp(timestamp);
   // TODO: ARPITA: Manipulate s to convert it to a bengali timestamp
 
-  String month = s.substring(13, 15); // why 13-15 ???
+  String month = s.substring(13, 16); // why 13-16 ???
   String bengaliMonth;
   switch (month) {
     case 'Jan':
@@ -21,6 +21,38 @@ String getTimeStampBengali(Timestamp timestamp) {
     case 'Feb':
       bengaliMonth = 'ফেব্রুয়ারি';
       break;
+    case 'Mar':
+      bengaliMonth = 'মার্চ';
+      break;
+    case 'Apr':
+      bengaliMonth = 'এপ্রিল';
+      break;
+    case 'May':
+      bengaliMonth = 'মে';
+      break;
+    case 'Jun':
+      bengaliMonth = 'জুন';
+      break;
+    case 'Jul':
+      bengaliMonth = 'জুলাই';
+      break;
+    case 'Aug':
+      bengaliMonth = 'আগষ্ট';
+      break;
+    case 'Sep':
+      bengaliMonth = 'সেপ্টেম্বর';
+      break;
+    case 'Oct':
+      bengaliMonth = 'অক্টোবর';
+      break;
+    case 'Nov':
+      bengaliMonth = 'নভেম্বর';
+      break;
+    case 'Dec':
+      bengaliMonth = 'ডিসেম্বর';
+      break;
+    default:
+      bengaliMonth += month;
     // TODO
   }
   debugPrint(bengaliMonth);
